b1d76bb2a1acdf34c09795dcee60addb02241874
Praful-rathore-222 <rathorepraful420@gmail.com>
2021-01-12 13:28:12 +0530

    change in admin.py files

--- ecom_site/posts/admin.py
+++ ecom_site/posts/admin.py
@@ -4,6 +4,19 @@ from .models import Post
 from .models import Comments
 
 # Register your models here.
-admin.site.register(Post)
+class CommentsInline(admin.StackedInline):
+    model = Comments
+    extra = 3
+
+
+class PostA(admin.ModelAdmin):
+    list_display = ("description", "date_posted")
+    search_fields = ["description"]
+    inlines = [CommentsInline]
+    # class meta :
+    #     model = Posts
+
+
+admin.site.register(Post, PostA)
 
 admin.site.register(Comments)
--- ecom_site/profile/admin.py
+++ ecom_site/profile/admin.py
@@ -3,5 +3,11 @@ from .models import Profile
 from .models import FriendRequest
 
 # Register your models here.
-admin.site.register(Profile)
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display =['first_name', 'last_name','image', 'bio', 'email', 'created']
+    list_filter =['first_name', 'last_name', 'email', 'created']
+
+
+admin.site.register(Profile, ProfileAdmin)
 admin.site.register(FriendRequest)
